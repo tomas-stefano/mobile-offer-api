1b14390381ebda2eba746c07408aaca9606b6368
Tomas D'Stefano <tomas_stefano@successoft.com>
2014-10-23 10:52:16 +0100

    Move API_KEY to the fyber yaml configuration file.

--- app/resources/fyber/hash_key.rb
+++ app/resources/fyber/hash_key.rb
@@ -2,14 +2,12 @@ module Fyber
   class HashKey
     attr_reader :params
 
-    API_KEY = 'b07a12df7d52e6c118e5d47d3f9e60135b109a1f'
-
     def initialize(request_params)
       @params = request_params.sort.to_h
     end
 
     def generate
-      Digest::SHA1.hexdigest("#{@params.to_query}&#{API_KEY}")
+      Digest::SHA1.hexdigest("#{@params.to_query}&#{Fyber::APISettings.api_key}")
     end
   end
 end
